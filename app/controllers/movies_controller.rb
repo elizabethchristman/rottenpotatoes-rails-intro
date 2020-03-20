c63788cc30425524c5b0213f6672b93a3727e917
elizabethchristman <elizabethchristman31@gmail.com>
2020-03-20 14:36:44 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,20 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    if params.key?(:sort_by)
-      session[:sort_by] = params[:sort_by]
-    elsif session.key?(:sort_by)
-      params[:sort_by] = session[:sort_by]
-			redirect_to movies_path(params) and return
-    end
     
     @all_ratings = Movie.all_ratings
-		if params.key?(:ratings)
-			session[:ratings] = params[:ratings]
-		elsif session.key?(:ratings)
-			params[:ratings] = session[:ratings]
-			redirect_to movies_path(params) and return
-		end
 	  @checked_ratings = (session[:ratings].keys if session.key?(:ratings)) || @all_ratings
 	  
 		@hilite = sort_by = session[:sort_by]
